65330b3f21445e8a8b8032fb7740de4fca635a8f
YOUNTAEHEE <f_hyang@naver.com>
2025-03-08 23:03:35 +0900

    git init

---
+++ .gitmodules.js
@@ -0,0 +1,6 @@
+[submodule "check-love-backend"]
+	path = check-love-backend
+	url = git@github.com:YOUNTAEHEE/love.git
+[submodule "check-love-frontend"]
+	path = check-love-frontend
+	url = git@github.com:YOUNTAEHEE/check-love.git
